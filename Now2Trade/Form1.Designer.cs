9a296c07bf8aba20f1f8923b4a53702c767ee6c5
Enes <eneskemel@gmail.com>
2022-04-05 09:24:10 +0300

    Add project files.

---
+++ Now2Trade/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Now2Trade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.tmrAna = new System.Windows.Forms.Timer(this.components);
+            this.listLog = new System.Windows.Forms.ListBox();
+            this.tmrIslem = new System.Windows.Forms.Timer(this.components);
+            this.lblDeger = new System.Windows.Forms.Label();
+            this.lblSonIslem = new System.Windows.Forms.Label();
+            this.tmrZamanHesaplama = new System.Windows.Forms.Timer(this.components);
+            this.lblKalanZaman = new System.Windows.Forms.Label();
+            this.txtFark = new System.Windows.Forms.TextBox();
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.lblFarkMax = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(12, 12);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 23);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // tmrAna
+            // 
+            this.tmrAna.Interval = 1000;
+            this.tmrAna.Tick += new System.EventHandler(this.tmrAna_Tick);
+            // 
+            // listLog
+            // 
+            this.listLog.FormattingEnabled = true;
+            this.listLog.Location = new System.Drawing.Point(93, 12);
+            this.listLog.Name = "listLog";
+            this.listLog.Size = new System.Drawing.Size(220, 264);
+            this.listLog.TabIndex = 1;
+            // 
+            // tmrIslem
+            // 
+            this.tmrIslem.Interval = 200;
+            this.tmrIslem.Tick += new System.EventHandler(this.tmrIslem_Tick);
+            // 
+            // lblDeger
+            // 
+            this.lblDeger.AutoSize = true;
+            this.lblDeger.Location = new System.Drawing.Point(12, 47);
+            this.lblDeger.Name = "lblDeger";
+            this.lblDeger.Size = new System.Drawing.Size(35, 13);
+            this.lblDeger.TabIndex = 2;
+            this.lblDeger.Text = "label1";
+            // 
+            // lblSonIslem
+            // 
+            this.lblSonIslem.AutoSize = true;
+            this.lblSonIslem.Location = new System.Drawing.Point(12, 117);
+            this.lblSonIslem.Name = "lblSonIslem";
+            this.lblSonIslem.Size = new System.Drawing.Size(35, 13);
+            this.lblSonIslem.TabIndex = 3;
+            this.lblSonIslem.Text = "label1";
+            // 
+            // tmrZamanHesaplama
+            // 
+            this.tmrZamanHesaplama.Interval = 1000;
+            this.tmrZamanHesaplama.Tick += new System.EventHandler(this.tmrZamanHesaplama_Tick);
+            // 
+            // lblKalanZaman
+            // 
+            this.lblKalanZaman.AutoSize = true;
+            this.lblKalanZaman.Location = new System.Drawing.Point(12, 72);
+            this.lblKalanZaman.Name = "lblKalanZaman";
+            this.lblKalanZaman.Size = new System.Drawing.Size(35, 13);
+            this.lblKalanZaman.TabIndex = 4;
+            this.lblKalanZaman.Text = "label1";
+            // 
+            // txtFark
+            // 
+            this.txtFark.Location = new System.Drawing.Point(-4, 88);
+            this.txtFark.Name = "txtFark";
+            this.txtFark.Size = new System.Drawing.Size(100, 20);
+            this.txtFark.TabIndex = 6;
+            this.txtFark.Text = "470";
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Interval = 1000;
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // lblFarkMax
+            // 
+            this.lblFarkMax.AutoSize = true;
+            this.lblFarkMax.Location = new System.Drawing.Point(12, 148);
+            this.lblFarkMax.Name = "lblFarkMax";
+            this.lblFarkMax.Size = new System.Drawing.Size(13, 13);
+            this.lblFarkMax.TabIndex = 7;
+            this.lblFarkMax.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 284);
+            this.Controls.Add(this.lblFarkMax);
+            this.Controls.Add(this.txtFark);
+            this.Controls.Add(this.lblKalanZaman);
+            this.Controls.Add(this.lblSonIslem);
+            this.Controls.Add(this.lblDeger);
+            this.Controls.Add(this.listLog);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Timer tmrAna;
+        private System.Windows.Forms.ListBox listLog;
+        private System.Windows.Forms.Timer tmrIslem;
+        private System.Windows.Forms.Label lblDeger;
+        private System.Windows.Forms.Label lblSonIslem;
+        private System.Windows.Forms.Timer tmrZamanHesaplama;
+        private System.Windows.Forms.Label lblKalanZaman;
+        private System.Windows.Forms.TextBox txtFark;
+        private System.Windows.Forms.Timer tmrTime;
+        private System.Windows.Forms.Label lblFarkMax;
+    }
+}
+
---
+++ Now2Trade/Form1.cs
@@ -0,0 +1,239 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Now2Trade
+{
+    public partial class Form1 : Form
+    {
+        private IWebDriver driver = new ChromeDriver();
+        string projectURL = "https://now2trade.com/arbitrage/en/";
+        string sleepURL = "https://google.com";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int deger = 0;
+        int islemBaslamaDurumu = 0;
+        int beklemeSuresi = -1;
+        int beklemeSuresiGeriSayim = 0;
+        int time = 0;
+        int loginCheckCount = 0;
+        private void tmrAna_Tick(object sender, EventArgs e)
+        {
+            deger++;
+            lblDeger.Text = deger.ToString();
+            if (deger == 2)
+            {
+                islemBaslamaDurumu = 0;
+                beklemeSuresi = 0;
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                driver.Navigate().GoToUrl(projectURL);
+            }
+            if (deger == 10)
+            {
+                driver.FindElement(By.Name("userId")).SendKeys("eno_dai@msn.com");
+                driver.FindElement(By.Name("passW")).SendKeys("1236asd");
+                driver.FindElement(By.Name("passW")).SendKeys(OpenQA.Selenium.Keys.Tab);
+                string Giris = "document.querySelector(\"#app > div.view.view-main.view-init.safe-areas > div.page.page-signin.page-current > div.page-content > div.block.bottom-button-container.HIDEONINPUTFOCUS > div > div > button\").click();";
+                jsCalistir(Giris);
+                listLog.Items.Add("Giris Yaptım");
+            }
+            if (deger == 20)
+            {
+                String html = driver.PageSource;
+                if (html.Contains("Enes KEMEL"))
+                {
+                    listLog.Items.Add("Giris Basarili");
+                }
+                else
+                {
+                    if (loginCheckCount < 3)
+                    {
+                        deger = 0;
+                        listLog.Items.Add("Giriş Başarısız.");
+                        loginCheckCount++;
+                    }
+                    else
+                    {
+                        tmrAna.Stop();
+                        listLog.Items.Add("Bir şeyler ters gitti login olamıyorum.");
+                    }
+                 
+                }
+            }
+            if (deger == 21)
+            {
+                string islemDurumu = driver.FindElement(By.CssSelector(".text-mute.small.text-secondary.mt-0.timelapes")).Text;
+                listLog.Items.Add(islemDurumu);
+                if (islemDurumu.Contains("Finished")) //Finished
+                {
+                    islemBaslamaDurumu = 1;
+                    listLog.Items.Add("İşleme Başlayacağım");
+                    string islemSayfasinaGec = "document.querySelector(\"#app > div.view.view-main.view-init.safe-areas > div.page.page-homepage.light.page-current > div.toolbar.tabbar.toolbar-bottom.o-visible.o-hidden.BOTTOM_MENUS > div > a.BOTTOMTABS.tab-link.exCh\").click()";
+                    jsCalistir(islemSayfasinaGec);
+                    listLog.Items.Add("İslem Sayfasina Geçtim");
+                }
+                else if (islemDurumu.Contains("hour")) //hour
+                {
+                    listLog.Items.Add("Bekleme saati oluşturacağım.");
+                    beklemeSuresi = islemDurumuKontrolFNC(islemDurumu);
+                    beklemeSuresiGeriSayim = beklemeSuresi;
+                    tmrZamanHesaplama.Start();
+                }
+                else
+                {
+                    tmrAna.Stop();
+                    listLog.Items.Add("Bir sikinti var." + islemDurumu);
+                }
+            }
+            if (deger == 26)
+            {
+                if (islemBaslamaDurumu == 1)
+                {
+                    string tumBakiyeSec = "document.querySelector(\"#exchanges > div:nth-child(1) > div > ul > li.item-content.item-input > div:nth-child(3) > button\").click()";
+                    jsCalistir(tumBakiyeSec);
+                    listLog.Items.Add("Bakiye Seçtim.");
+                    tmrIslem.Start();
+                    tmrAna.Stop();
+                    tmrTime.Start();
+                }
+            }
+            if (deger == 30)
+            {
+                if (beklemeSuresi < 1)
+                {
+                    deger = 0;
+                }
+            }
+            if (deger == beklemeSuresi)
+            {
+                islemBaslamaDurumu = 0;
+                beklemeSuresi = 0;
+                deger = 0;
+                listLog.Items.Clear();
+            }
+        }
+        private void jsCalistir(string jsString)
+        {
+            try
+            {
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript(jsString);
+            }
+            catch (Exception ex)
+            {
+                listLog.Items.Add(ex.ToString());
+            }
+
+        }
+        double farkKontrol = 0;
+        private void tmrIslem_Tick(object sender, EventArgs e)
+        {
+            listLog.SelectedIndex = listLog.Items.Count - 1;
+            try
+            {
+                try
+                {
+                    String fark = driver.FindElement(By.CssSelector(".farq2")).Text;
+                    String[] farkList = fark.Split(' ');
+                    farkKontrol = System.Convert.ToDouble(farkList[0]);
+                    listLog.Items.Add(farkKontrol + " Fark durumu ");
+                    listLog.SelectedIndex = listLog.Items.Count - 1;
+                    if (farkKontrol > Convert.ToDouble(lblFarkMax.Text))
+                    {
+                        lblFarkMax.Text = farkKontrol.ToString();
+                    }
+                    if (farkKontrol > Convert.ToInt16(txtFark.Text))
+                    {
+                        lblSonIslem.Text = (farkKontrol + " Fark durumu " + DateTime.Now.Hour.ToString() + " " + DateTime.Now.Minute.ToString() + " " + DateTime.Now.Second.ToString());
+                        string islemYap = "document.querySelector(\"#exchanges > div:nth-child(2) > div.row.PRICELESS > table > tbody > tr:nth-child(7) > td.startButtons > button\").click()";
+                        jsCalistir(islemYap);
+                        listLog.Items.Add("İşlemi Yaptım");
+                        tmrIslem.Stop();
+                        tmrAna.Start();
+                        tmrTime.Stop();
+                        time = 0;
+                    }
+                }
+                catch (Exception exx)
+                {
+                    listLog.Items.Add(exx.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                listLog.Items.Add(ex.ToString());
+            }
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            tmrAna.Start();
+        }
+
+        private int islemDurumuKontrolFNC(string islemSaati)
+        {
+            String[] list = islemSaati.Split(' ');
+            int beklemeSaniyesi = 0;
+            int saat = Convert.ToInt32(list[0]);
+            int dakika = Convert.ToInt32(list[2]);
+            int saniye = Convert.ToInt32(list[4]);
+            beklemeSaniyesi = saat * 60 * 60;
+            beklemeSaniyesi += dakika * 60;
+            beklemeSaniyesi += saniye;
+
+            Random rdm = new Random();
+            int rdmDeger = rdm.Next(1, 1000);
+            beklemeSaniyesi += rdmDeger;
+
+            listLog.Items.Add(beklemeSaniyesi / 60 + " Dakika Sonra");
+
+            driver.Navigate().GoToUrl(sleepURL);
+            return beklemeSaniyesi;
+        }
+
+        private void tmrZamanHesaplama_Tick(object sender, EventArgs e)
+        {
+            if (beklemeSuresiGeriSayim > 1)
+            {
+                lblKalanZaman.Text = beklemeSuresiGeriSayim / 60 + " Kalan dakika";
+                beklemeSuresiGeriSayim--;
+            }
+            else
+            {
+                tmrZamanHesaplama.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+            time++;
+            lblDeger.Text = "İşlem Yapılmadan geçen süre " + time;
+            if (time == 600)
+            {
+                time = 0;
+                deger = 0;
+                islemBaslamaDurumu = 0;
+                beklemeSuresi = -1;
+                beklemeSuresiGeriSayim = 0;
+                tmrTime.Stop();
+                tmrIslem.Stop();
+                tmrAna.Start();
+            }
+        }
+    }
+}
